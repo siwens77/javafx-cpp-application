7cb14b6bf9ceaaea813486230d89493176a47371
krzychu <krzysztof.nowak2005@gmail.com>
2026-01-02 21:13:27 +0100

    cpp tests are donegit status

--- src/Main.java
+++ src/Main.java
@@ -9,22 +9,19 @@ import javafx.scene.image.Image;
 public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Font.loadFont(getClass().getResource("resources/fonts/font.otf").toExternalForm(), 16);
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("mainscene.fxml"));
+        Font.loadFont(getClass().getResource("/resources/fonts/font.otf").toExternalForm(), 16);
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("/mainscene.fxml"));
         Parent root = loader.load();
         Controller controller = loader.getController();
 
 
-        Image image = new Image("resources/images/icon.png");
+        Image image = new Image("/resources/images/icon.png");
         primaryStage.getIcons().add(image);
 
         controller.setMainWindow(primaryStage);
         primaryStage.setTitle("Cats Battle");
         primaryStage.setScene(new Scene(root, 800,578));
         primaryStage.show();
-
-
-
     }
 
     public static void main(String[] args) {
